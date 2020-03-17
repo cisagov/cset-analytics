a7a010e13de501716bbc68465e8181bba81f5c5b
Jason Kuipers <kuipers.jason@gmail.com>
2020-03-17 13:57:04 -0600

    add business for dashboard

---
+++ CsetAnalytics.Business/Dashboard/DashboardBusiness.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using CsetAnalytics.DomainModels;
+using CsetAnalytics.Interfaces.Dashboard;
+using CsetAnalytics.ViewModels.Dashboard;
+
+namespace CsetAnalytics.Business.Dashboard
+{
+    public class DashboardBusiness : IDashboardBusiness
+    {
+        private readonly CsetContext _context;
+
+        public DashboardBusiness(CsetContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Series>> GetSectionAnalytics(string section)
+        {
+            return null;
+        }
+
+        public async Task<List<Series>> GetIndustryAnalytics(string industry)
+        {
+            return null;
+        }
+
+        public async Task<List<Series>> GetMyAnalytics(int assessmentId)
+        {
+            return null;
+        }
+    }
+}
