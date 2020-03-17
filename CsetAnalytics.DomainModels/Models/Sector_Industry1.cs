9d16d1eaef3d6bb67116d49c64c7ede626cdc1b3
INLGuy <5023364+inlguy@users.noreply.github.com>
2020-03-17 14:07:59 -0600

    updated database

---
+++ CsetAnalytics.DomainModels/Models/Sector_Industry1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace CsetAnalytics.DomainModels.Models
+{
+
+
+    [Table("SECTOR")]
+    public partial class SECTOR
+    {
+
+        public SECTOR()
+        {
+            Sector_Industry = new HashSet<Sector_Industry>();
+        }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int SectorId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string SectorName { get; set; }
+
+
+        public virtual ICollection<Sector_Industry> Sector_Industry { get; set; }
+    }
+
+    public partial class Sector_Industry
+    {
+
+        public Sector_Industry()
+        {
+            Assessments = new HashSet<Assessment>();
+        }
+
+        
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int SectorId { get; set; }
+
+        
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int IndustryId { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string IndustryName { get; set; }
+
+
+        public virtual ICollection<Assessment> Assessments { get; set; }
+
+        public virtual SECTOR SECTOR { get; set; }
+    }
+}
