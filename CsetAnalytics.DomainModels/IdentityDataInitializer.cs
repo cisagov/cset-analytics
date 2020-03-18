d9e6ae2363746d6057400d44fd2da1bf75010d32
Jason Kuipers <kuipers.jason@gmail.com>
2020-03-18 14:57:04 -0600

    remove user initializer

--- CsetAnalytics.DomainModels/IdentityDataInitializer.cs
+++ CsetAnalytics.DomainModels/IdentityDataInitializer.cs
@@ -12,36 +12,6 @@ namespace CsetAnalytics.DomainModels
         public static async Task SeedData(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, CsetContext context)
         {
             await SeedRoles(userManager, roleManager);
-            await SeedUsers(userManager, context);
-
-        }
-
-        public static async Task SeedUsers(UserManager<ApplicationUser> userManager, CsetContext context)
-        {
-
-            if (await userManager.FindByNameAsync
-                    ("jkuipers") == null)
-            {
-                ApplicationUser user = new ApplicationUser();
-                user.UserName = "jkuipers";
-                user.Email = "jkuipers@inl.gov";
-
-                IdentityResult result = userManager.CreateAsync(user, "P@ssw0rd!").Result;
-                if (result.Succeeded)
-                {
-                    await userManager.AddToRoleAsync(user, RolesEnum.Admin.ToString());
-                }
-            }
-
-            var users = context.Users;
-            foreach (var u in users)
-            {
-                var role = context.UserRoles.FirstOrDefault(x => x.UserId == u.Id);
-                if (role == null)
-                {
-                    await userManager.AddToRoleAsync(u, RolesEnum.User.ToString());
-                }
-            }
         }
 
         public static async Task SeedRoles(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
